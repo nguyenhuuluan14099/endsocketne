09f34e0077be3ddf21c54f925ba5e13b9fc1151e
Nguyen Luan <nguyenluan14099@gmail.com>
2024-05-12 21:26:40 +0700

    changeUrl

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socialclient.vercel.app",
     methods: ["GET", "POST"],
   },
 });
